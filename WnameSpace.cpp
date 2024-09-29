15da0654c11283602e8dc0a653a12d76da9dc8ff
yanw57 <305072048@qq.com>
2024-09-29 22:02:06 +0800

    namespaceReview

---
+++ WnameSpace.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+//声明命名空间std
+using namespace std;
+
+void func(){
+    cout<<"http://c.biancheng.net"<<endl;
+}
+
+int main(){
+    cout<<"C语言中文网"<<endl;
+    func();
+
+    return 0;
+}
